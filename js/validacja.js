bde5c593176618ee46b5d45d87f3ba34ee698a16
pawelowsianko <p.owsianko04@gmail.com>
2021-12-21 13:16:41 +0100

    szkielet strony oraz połowicznie działajaca validacja

---
+++ js/validacja.js
@@ -0,0 +1,15 @@
+var x = document.getElementById('x').value
+function validacjax(){
+    if (x == ""){
+        alert("uzupełnij liczbe x");
+        return false;
+    }
+}
+
+var y = document.getElementById('y').value
+function validacjay(){
+    if (y == ""){
+        alert("uzupełnij liczbe y");
+        return false;
+    }
+}
